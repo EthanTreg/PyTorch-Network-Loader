454d566bfcf0778146db21dde573dbd52bf03b8d
Ethan Tregidga <ethan@treggal.com>
2023-10-17 16:16:29 +0200

    Added setup.py for pip installing. Added credits to __init__.py.

---
+++ netloader/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package information
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Ethan Tregidga'
---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+Package setup
+"""
+from setuptools import setup
+
+setup(
+    name='netloader',
+    version='0.1.0',
+    description='Utility to generate PyTorch neural network objects from JSON files',
+    url='https://github.com/EthanTreg/PyTorch-Network-Loader',
+    author='Ethan Tregidga',
+    author_email='et1g19@soton.ac.uk',
+    license='MIT',
+    packages=['netloader'],
+    install_requires=['torch'],
+)
