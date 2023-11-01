Fixed network construction errors due to incorrect shapes. Removed squeeze layer as reshape achieves the same.

netloader/layers/convolutional.py
@@ -55,6 +55,11 @@ class PixelShuffle1d(nn.Module):
         return x
 
 
+def _conv_shape(shape: list[int], padding: int, kernel_size: int, stride: int):
+    shape[1:] = [int((length + 2 * padding - kernel_size) / stride + 1) for length in shape[1:]]
+    return shape
+
+
 def convolutional(kwargs: dict, layer: dict) -> dict:
     """
     Convolutional layer constructor
@@ -98,7 +103,8 @@ def convolutional(kwargs: dict, layer: dict) -> dict:
     kernel_size = 3
     stride = 1
     padding = 'same'
-    kwargs['dims'].append(layer['filters'])
+    shape = kwargs['shape'][-1].copy()
+    shape[0] = layer['filters']
 
     # Optional parameters
     if 'kernel' in layer:
@@ -120,8 +126,8 @@ def convolutional(kwargs: dict, layer: dict) -> dict:
         batch_norm = nn.BatchNorm1d
 
     conv = conv(
-        in_channels=kwargs['dims'][-2],
-        out_channels=kwargs['dims'][-1],
+        in_channels=kwargs['shape'][-1][0],
+        out_channels=shape[0],
         kernel_size=kernel_size,
         stride=stride,
         padding=padding,
@@ -132,15 +138,13 @@ def convolutional(kwargs: dict, layer: dict) -> dict:
 
     # Optional layers
     optional_layer(True, 'dropout', kwargs, layer, dropout(kwargs['dropout_prob']))
-    optional_layer(False, 'batch_norm', kwargs, layer, batch_norm(kwargs['dims'][-1]))
+    optional_layer(False, 'batch_norm', kwargs, layer, batch_norm(shape[0]))
     optional_layer(True, 'activation', kwargs, layer, nn.ELU())
 
     if padding != 'same':
-        kwargs['data_size'].append(int(
-            (kwargs['data_size'][-1] + 2 * padding - kernel_size) / stride + 1
-        ))
+        kwargs['shape'].append(_conv_shape(shape, padding, kernel_size, stride))
     else:
-        kwargs['data_size'].append(kwargs['data_size'][-1])
+        kwargs['shape'].append(shape)
 
     return kwargs
 
@@ -190,11 +194,10 @@ def conv_upscale(kwargs: dict, layer: dict) -> dict:
 
     # Upscaling done using pixel shuffling
     kwargs['module'].add_module(f"pixel_shuffle_{kwargs['i']}", pixel_shuffle(2))
-    kwargs['dims'][-1] = int(kwargs['dims'][-1] / 2)
+    kwargs['shape'][-1][0] = int(kwargs['shape'][-1][0] / 2)
 
     # Data size doubles
-    kwargs['data_size'].append(kwargs['data_size'][-1] * 2)
-
+    kwargs['shape'][-1][1:] = [length * 2 for length in kwargs['shape'][-1][1:]]
     return kwargs
 
 
@@ -232,7 +235,8 @@ def conv_transpose(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(layer['filters'])
+    kwargs['shape'].append(kwargs['shape'][-1])
+    kwargs['shape'][-1][0] = layer['filters']
 
     if ('2d' in layer and layer['2d']) or ('2d' not in layer and kwargs['2d']):
         transpose = nn.ConvTranspose2d
@@ -244,8 +248,8 @@ def conv_transpose(kwargs: dict, layer: dict) -> dict:
         batch_norm = nn.BatchNorm1d
 
     conv = transpose(
-        in_channels=kwargs['dims'][-2],
-        out_channels=kwargs['dims'][-1],
+        in_channels=kwargs['shape'][-2][0],
+        out_channels=kwargs['shape'][-1][0],
         kernel_size=2,
         stride=2,
     )
@@ -254,12 +258,11 @@ def conv_transpose(kwargs: dict, layer: dict) -> dict:
 
     # Optional layers
     optional_layer(True, 'dropout', kwargs, layer, dropout(kwargs['dropout_prob']))
-    optional_layer(False, 'batch_norm', kwargs, layer, batch_norm(kwargs['dims'][-1]))
+    optional_layer(False, 'batch_norm', kwargs, layer, batch_norm(kwargs['shape'][-1][0]))
     optional_layer(True, 'activation', kwargs, layer, nn.ELU())
 
     # Data size doubles
-    kwargs['data_size'].append(kwargs['data_size'][-1] * 2)
-
+    kwargs['shape'][-1][1:] = [length * 2 for length in kwargs['shape'][-1][1:]]
     return kwargs
 
 
@@ -296,7 +299,6 @@ def conv_depth_downscale(kwargs: dict, layer: dict) -> dict:
     layer['padding'] = 'same'
 
     convolutional(kwargs, layer)
-
     return kwargs
 
 
@@ -339,7 +341,6 @@ def conv_downscale(kwargs: dict, layer: dict) -> dict:
     layer['padding'] = 1
 
     convolutional(kwargs, layer)
-
     return kwargs
 
 
@@ -367,7 +368,7 @@ def pool(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(int(kwargs['dims'][-1]))
+    kwargs['shape'].append(kwargs['shape'][-1])
 
     if ('2d' in layer and layer['2d']) or ('2d' not in layer and kwargs['2d']):
         max_pool = nn.MaxPool2d
@@ -377,6 +378,5 @@ def pool(kwargs: dict, layer: dict) -> dict:
     kwargs['module'].add_module(f"pool_{kwargs['i']}", max_pool(kernel_size=2))
 
     # Data size halves
-    kwargs['data_size'].append(int(kwargs['data_size'][-1] / 2))
-
+    kwargs['shape'][-1][1:] = [int(length / 2) for length in kwargs['shape'][-1][1:]]
     return kwargs

netloader/layers/linear.py
@@ -2,10 +2,11 @@
 Linear network layers
 """
 import torch
+import numpy as np
 from torch import nn, Tensor
 
 from netloader.utils.utils import get_device
-from netloader.layers.utils import optional_layer
+from netloader.layers.utils import Reshape, optional_layer
 
 
 class Sample(nn.Module):
@@ -105,23 +106,32 @@ def linear(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    # Number of features can be defined by either a factor of the output size or explicitly
-    try:
-        kwargs['dims'].append(int(kwargs['output_size'] * layer['factor']))
-    except KeyError:
-        kwargs['dims'].append(layer['features'])
+    # Remove channels dimension
+    if len(kwargs['shape'][-1]) > 1:
+        kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape([-1]))
 
-    linear_layer = nn.Linear(in_features=kwargs['dims'][-2], out_features=kwargs['dims'][-1])
+    # Number of features can be defined by either a factor of the output size or explicitly
+    if 'factor' in layer:
+        out_features = int(np.prod(kwargs['output_shape']) * layer['factor'])
+    else:
+        out_features = layer['features']
+
+    linear_layer = nn.Linear(in_features=np.prod(kwargs['shape'][-1]), out_features=out_features)
     kwargs['module'].add_module(f"linear_{kwargs['i']}", linear_layer)
 
     # Optional layers
     optional_layer(False, 'dropout', kwargs, layer, nn.Dropout1d(kwargs['dropout_prob']))
-    optional_layer(False, 'batch_norm', kwargs, layer, nn.BatchNorm1d(kwargs['dims'][-1]))
+    optional_layer(False, 'batch_norm', kwargs, layer, nn.BatchNorm1d(out_features))
     optional_layer(True, 'activation', kwargs, layer, nn.SELU())
 
-    # Data size equals number of nodes
-    kwargs['data_size'].append(kwargs['dims'][-1])
+    # Add channels dimension equal to input channels if input contains channels
+    if len(kwargs['shape'][-1]) > 1:
+        out_shape = [kwargs['shape'][-1][0], int(out_features / kwargs['shape'][-1][0])]
+        kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape(out_shape))
+    else:
+        out_shape = [out_features]
 
+    kwargs['shape'].append(out_shape)
     return kwargs
 
 
@@ -148,14 +158,13 @@ def upsample(kwargs: dict, _: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(kwargs['dims'][-1])
+    kwargs['shape'].append(kwargs['shape'][-1])
     kwargs['module'].add_module(
         f"upsample_{kwargs['i']}",
         nn.Upsample(scale_factor=2, mode='linear')
     )
     # Data size doubles
-    kwargs['data_size'].append(kwargs['data_size'][-1] * 2)
-
+    kwargs['shape'][-1][1:] = [length * 2 for length in kwargs['shape'][-1][1:]]
     return kwargs
 
 
@@ -190,15 +199,22 @@ def sample(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
+    # Remove channels dimension
+    kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape([-1]))
+
     # Number of features can be defined by either a factor of the output size or explicitly
     try:
-        kwargs['dims'].append(int(kwargs['output_size'] * layer['factor']))
+        out_features = int(np.prod(kwargs['output_shape']) * layer['factor'])
     except KeyError:
-        kwargs['dims'].append(layer['features'])
+        out_features = layer['features']
 
-    kwargs['data_size'].append(kwargs['dims'][-1])
     kwargs['module'].add_module(
         f"sample_{kwargs['i']}",
-        Sample(kwargs['dims'][-2], kwargs['dims'][-1]),
+        Sample(np.prod(kwargs['shape'][-1]), out_features),
     )
+
+    # Add channels dimension equal to input channels
+    out_shape = [kwargs['shape'][-1][0], int(out_features / kwargs['shape'][-1][0])]
+    kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape(out_shape))
+    kwargs['data_shape'].append(out_shape)
     return kwargs

netloader/layers/recurrent.py
@@ -2,9 +2,10 @@
 Recurrent network layers
 """
 import torch
+import numpy as np
 from torch import nn, Tensor
 
-from netloader.layers.utils import optional_layer
+from netloader.layers.utils import Reshape, optional_layer
 
 
 class RecurrentOutput(nn.Module):
@@ -109,22 +110,21 @@ def recurrent(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    try:
+    kwargs['shape'].append([kwargs['shape'][-1][0], np.prod(kwargs['shape'][-1][1:])])
+
+    if 'layers' in layer:
         layers = layer['layers']
-    except KeyError:
+    else:
         layers = 2
 
-    try:
-        kwargs['dims'].append(layer['filters'])
-    except KeyError:
-        kwargs['dims'].append(1)
+    if 'filters' in layer:
+        kwargs['shape'][-1][0] = layer['filters']
+    else:
+        kwargs['shape'][-1][0] = 1
 
-    try:
+    if 'bidirectional' in layer and layer['bidirectional'].lower() != 'none':
         bidirectional = layer['bidirectional']
-
-        if bidirectional == 'None':
-            bidirectional = None
-    except KeyError:
+    else:
         bidirectional = None
 
     if layers > 1 and (('dropout' in layer and layer['dropout']) or 'dropout' not in layer):
@@ -133,8 +133,8 @@ def recurrent(kwargs: dict, layer: dict) -> dict:
         dropout_prob = 0
 
     recurrent_kwargs = {
-        'input_size': kwargs['dims'][-2],
-        'hidden_size': kwargs['dims'][-1],
+        'input_size': kwargs['shape'][-1][0],
+        'hidden_size': kwargs['shape'][-1][0],
         'num_layers': layers,
         'batch_first': True,
         'dropout': dropout_prob,
@@ -142,26 +142,30 @@ def recurrent(kwargs: dict, layer: dict) -> dict:
     }
 
     if bidirectional == 'concatenate':
-        kwargs['dims'][-1] *= 2
+        kwargs['shape'][-1][0] *= 2
 
-    try:
-        if layer['method'] == 'rnn':
-            recurrent_layer = nn.RNN(**recurrent_kwargs, nonlinearity='relu')
-        elif layer['method'] == 'lstm':
-            recurrent_layer = nn.LSTM(**recurrent_kwargs)
-        else:
-            recurrent_layer = nn.GRU(**recurrent_kwargs)
-    except KeyError:
+    if 'method' in layer and layer['method'] == 'rnn':
+        recurrent_layer = nn.RNN(**recurrent_kwargs, nonlinearity='relu')
+    elif 'method' in layer and layer['method'] == 'lstm':
+        recurrent_layer = nn.LSTM(**recurrent_kwargs)
+    else:
         recurrent_layer = nn.GRU(**recurrent_kwargs)
 
+    # Convert 2D data to 1D and add recurrent layer
+    kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape([kwargs['shape'][-1][0], -1]))
     kwargs['module'].add_module(
-        f"GRU_output_{kwargs['i']}",
+        f"recurrent_{kwargs['i']}",
         RecurrentOutput(recurrent_layer, bidirectional=bidirectional)
     )
 
-    optional_layer(False, 'batch_norm', kwargs, layer, nn.BatchNorm1d(kwargs['dims'][-1]))
+    # Optional layers
+    optional_layer(
+        False,
+        'batch_norm',
+        kwargs,
+        layer,
+        nn.BatchNorm1d(kwargs['shape'][-1][0])
+    )
     optional_layer(True, 'activation', kwargs, layer, nn.ELU())
-
-    kwargs['data_size'].append(kwargs['data_size'][-1])
 
     return kwargs

netloader/layers/utils.py
@@ -1,7 +1,7 @@
 """
 Miscellaneous network layers
 """
-import torch
+import numpy as np
 from torch import nn, Tensor
 
 
@@ -43,57 +43,6 @@ class Reshape(nn.Module):
             Output tensor
         """
         return x.contiguous().view(x.size(0), *self.shape)
-
-
-class Squeeze(nn.Module):
-    """
-    Squeezes or un-squeezes a dimension
-
-    Attributes
-    ----------
-    remove : boolean
-        If dimension should be squeezed or un-squeezed
-    dim : integer
-        Target dimension
-
-    Methods
-    -------
-    forward(x)
-        Forward pass of squeeze
-    """
-    def __init__(self, remove: bool, dim: int):
-        """
-        Parameters
-        ----------
-        remove : boolean
-            If dimension should be squeezed or un-squeezed
-        dim : integer
-            Target dimension
-        """
-        super().__init__()
-        self.remove = remove
-        self.dim = dim
-
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Forward pass of squeeze
-
-        Parameters
-        ----------
-        x : Tensor
-            Input tensor
-
-        Returns
-        -------
-        Tensor
-            Output tensor
-        """
-        if self.remove:
-            x = torch.squeeze(x, self.dim)
-        else:
-            x = torch.unsqueeze(x, self.dim)
-
-        return x
 
 
 class Index(nn.Module):
@@ -197,63 +146,33 @@ def reshape(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    # If reshape reduces the number of dimensions
-    if len(layer['output']) == 1:
-        kwargs['dims'].append(kwargs['data_size'][-1] * kwargs['dims'][-1])
-
-        # Data size equals the previous size multiplied by the previous dimension
-        kwargs['data_size'].append(kwargs['dims'][-1])
+    # If -1 in output shape, calculate the dimension length from the input dimensions
+    if -1 not in layer['output']:
+        kwargs['shape'].append(layer['output'])
+    elif layer['output'].count(-1) == 1:
+        shape = layer['output'].copy()
+        fixed_shape = np.delete(shape, np.array(shape) == -1)
+        shape[shape.index(-1)] = int(np.prod(kwargs['shape'][-1]) / np.prod(fixed_shape))
+        kwargs['shape'].append(shape)
     else:
-        kwargs['dims'].append(layer['output'][0])
+        raise ValueError(
+            f"Cannot infer output shape as -1 occurs more than once in {layer['output']}"
+        )
 
-        # Data size equals the previous size divided by the first shape dimension
-        kwargs['data_size'].append(int(kwargs['data_size'][-1] / kwargs['dims'][-1]))
+    # If input tensor cannot be reshaped into output shape
+    if np.prod(kwargs['shape'][-1]) != np.prod(kwargs['shape'][-2]):
+        raise ValueError(
+            f"Output size does not match input size"
+            f"for input shape {kwargs['shape'][-2]} and output shape {kwargs['shape'][-1]}"
+        )
 
     kwargs['module'].add_module(f"reshape_{kwargs['i']}", Reshape(layer['output']))
-
-    return kwargs
-
-
-def squeeze(kwargs: dict, layer: dict) -> dict:
-    """
-    Squeezes or un-squeezes a dimension of the tensor
-
-    Parameters
-    ----------
-    kwargs : dictionary
-        i : integer
-            Layer number;
-        data_size : list[integer]
-            Hidden layer output length;
-        dims : list[integer]
-            Dimensions in each layer, either linear output features or convolutional/GRU filters;
-        module : Sequential
-            Sequential module to contain the layer;
-    layer : dictionary
-        squeeze : boolean
-            If dimension should be removed (True) or added (False);
-        dim : integer
-            Which dimension should be edited
-
-    Returns
-    -------
-    dictionary
-        Returns the input kwargs with any changes made by the function
-    """
-    kwargs['module'].add_module(f"squeeze_{kwargs['i']}", Squeeze(layer['squeeze'], layer['dim']))
-    kwargs['data_size'].append(kwargs['data_size'][-1])
-
-    if layer['squeeze']:
-        kwargs['dims'].append(kwargs['data_size'][-1])
-    else:
-        kwargs['dims'].append(1)
-
     return kwargs
 
 
 def extract(kwargs: dict, layer: dict) -> dict:
     """
-    Extracts a number of values from the tensor, returning two tensors
+    Extracts a number of values from the last dimension, returning two tensors
 
     Parameters
     ----------
@@ -271,8 +190,8 @@ def extract(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(kwargs['dims'][-1] - layer['number'])
-    kwargs['data_size'].append(kwargs['dims'][-1])
+    kwargs['shape'].append(kwargs['shape'][-1])
+    kwargs['shape'][-1][-1] -= layer['number']
     return kwargs
 
 
@@ -295,14 +214,13 @@ def clone(kwargs: dict, _: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(kwargs['dims'][-1])
-    kwargs['data_size'].append(kwargs['data_size'][-1])
+    kwargs['shape'].append(kwargs['shape'][-1])
     return kwargs
 
 
 def index(kwargs: dict, layer: dict) -> dict:
     """
-    Constructs a layer to slice the output from the previous layer
+    Constructs a layer to slice the last dimension from the output from the previous layer
 
     Parameters
     ----------
@@ -324,22 +242,21 @@ def index(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    kwargs['dims'].append(kwargs['dims'][-1])
+    kwargs['shape'].append(kwargs['shape'][-1])
 
+    # Length of slice
     if 'greater' in layer and (
             (layer['greater'] and layer['number'] < 0) or
             (not layer['greater'] and layer['number'] > 0)
     ):
-        data_size = abs(layer['number'])
+        kwargs['shape'][-1][-1] = abs(layer['number'])
     else:
-        data_size = kwargs['data_size'][-1] - abs(layer['number'])
+        kwargs['shape'][-1][-1] = kwargs['shape'][-1][-1] - abs(layer['number'])
 
-    kwargs['data_size'].append(data_size)
     kwargs['module'].add_module(
         f"index_{kwargs['i']}",
         Index(layer['number'], greater=layer['greater']),
     )
-
     return kwargs
 
 
@@ -357,21 +274,35 @@ def concatenate(kwargs: dict, layer: dict) -> dict:
     layer : dictionary
         layer : integer
             Layer index to concatenate the previous layer output with
-        channel : boolean
-            If concatenation should be performed over the channels or last dimension
+        dim : integer, default = 0
+            Dimension to concatenate to
 
     Returns
     -------
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    if 'channel' not in layer or layer['channel']:
-        kwargs['dims'].append(kwargs['dims'][-1] + kwargs['dims'][layer['layer']])
-        kwargs['data_size'].append(kwargs['data_size'][-1])
-    else:
-        kwargs['data_size'].append(kwargs['data_size'][-1] + kwargs['data_size'][layer['layer']])
-        kwargs['dims'].append(kwargs['data_size'][-1])
+    shape = kwargs['shape'][-1].copy()
+    target_shape = kwargs['shape'][layer['layer']].copy()
 
+    if 'dim' not in layer:
+        dim = 0
+    else:
+        dim = layer['dim']
+
+    in_shape = shape
+    del target_shape[dim]
+    del in_shape[dim]
+
+    # If tensors cannot be concatenated along the specified dimension
+    if target_shape != in_shape:
+        raise ValueError(
+            f"Shape mismatch, input shape {shape} does not match the "
+            f"target shape {kwargs['shape'][layer['layer']]} for concatenation over dimension {dim}"
+        )
+
+    shape[dim] = kwargs['shape'][-1][dim] + kwargs['shape'][layer['layer']][dim]
+    kwargs['shape'].append(shape)
     return kwargs
 
 
@@ -395,12 +326,26 @@ def shortcut(kwargs: dict, layer: dict) -> dict:
     dictionary
         Returns the input kwargs with any changes made by the function
     """
-    if kwargs['dims'][-1] == 1:
-        kwargs['dims'].append(kwargs['dims'][layer['layer']])
-    else:
-        kwargs['dims'].append(kwargs['dims'][-1])
+    shape = np.array(kwargs['shape'][-1]).copy()
+    mask = (kwargs['shape'][-1] != 1) & (kwargs['shape'][layer['layer']] != 1)
 
-    kwargs['data_size'].append(kwargs['data_size'][-1])
+    if not np.array_equal(kwargs['shape'][-1][mask], kwargs['shape'][layer['layer']][mask]):
+        raise ValueError(
+            f"Tensor shapes {kwargs['shape'][-1]} and "
+            f"{kwargs['shape'][layer['layer']]} not compatible for addition."
+        )
+
+    # If input has any dimensions of length one, output will take the target dimension
+    if 1 in kwargs['shape'][-1]:
+        idxs = np.where(np.array(kwargs['shape'][-1]) == 1)[0]
+        shape[idxs] = np.array(kwargs['shape'][layer['layer']])[idxs]
+
+    # If target has any dimensions of length one, output will take the input dimension
+    if 1 in kwargs['shape'][layer['layer']]:
+        idxs = np.where(np.array(kwargs['shape'][layer['layer']]) == 1)[0]
+        shape[idxs] = np.array(kwargs['shape'][-1])[idxs]
+
+    kwargs['shape'].append(shape)
     return kwargs
 
 
@@ -419,6 +364,5 @@ def skip(kwargs: dict, layer: dict) -> dict:
         layer : integer
             Layer index to retrieve the output;
     """
-    kwargs['dims'].append(kwargs['dims'][layer['layer']])
-    kwargs['data_size'].append(kwargs['data_size'][layer['layer']])
+    kwargs['shape'].append(kwargs['shape'][layer['layer']])
     return kwargs

netloader/network.py
@@ -2,7 +2,6 @@
 Constructs a network from layers and can load weights to resume network training
 """
 import json
-import logging as log
 
 import torch
 from torch import nn, optim, Tensor
@@ -47,18 +46,18 @@ class Network(nn.Module):
     """
     def __init__(
             self,
-            in_size: int,
-            out_size: int,
+            in_shape: list[int],
+            out_shape: list[int],
             learning_rate: float,
             name: str,
             config_dir: str):
         """
         Parameters
         ----------
-        in_size : integer
-            Size of the input tensor
-        out_size : integer
-            Size of the output tensor
+        in_shape : list[integer]
+            Shape of the input tensor
+        out_shape : list[integer]
+            shape of the output tensor
         learning_rate : float
             Optimizer initial learning rate
         name : string
@@ -77,8 +76,8 @@ class Network(nn.Module):
 
         # Construct layers in network
         self.layers, self.network = _create_network(
-            in_size,
-            out_size,
+            in_shape,
+            out_shape,
             f'{config_dir}{name}.json',
         )
 
@@ -141,18 +140,18 @@ class Network(nn.Module):
 
 
 def _create_network(
-        in_size: int | tuple[int, int],
-        out_size: int | tuple[int, int],
+        in_shape: list[int],
+        out_shape: list[int],
         config_path: str) -> tuple[list[dict], nn.ModuleList]:
     """
     Creates a network from a config file
 
     Parameters
     ----------
-    in_size : integer
-        Size of the input
-    out_size : integer
-        Size of the spectra
+    in_shape : integer
+        Shape of the input tensor
+    out_shape : integer
+        Shape of the output tensor
     config_path : string
         Path to the config file
 
@@ -168,19 +167,10 @@ def _create_network(
     if '2d' not in file['net']:
         file['net']['2d'] = False
 
-    if isinstance(in_size, tuple) and len(in_size) == 2:
-        dims, in_size = in_size
-    else:
-        dims = in_size
-
-    if isinstance(out_size, tuple):
-        out_size = out_size[-1]
-
     # Initialize variables
     kwargs = {
-        'data_size': [in_size],
-        'output_size': out_size,
-        'dims': [dims],
+        'shape': [in_shape],
+        'output_shape': out_shape,
         **file['net'],
     }
     module_list = nn.ModuleList()
@@ -189,23 +179,14 @@ def _create_network(
     for i, layer in enumerate(file['layers']):
         kwargs['i'] = i
         kwargs['module'] = nn.Sequential()
-
-        try:
-            kwargs = getattr(layers, layer['type'])(kwargs, layer)
-        except AttributeError as error:
-            log.error(f"Unknown layer: {layer['type']}")
-            raise error
-
+        kwargs = getattr(layers, layer['type'])(kwargs, layer)
         module_list.append(kwargs['module'])
 
     # Check network output dimensions
-    if kwargs['data_size'][-1] != out_size:
-        log.error(
-            f"Network output size ({kwargs['data_size']}) != data output size ({out_size})",
+    if kwargs['shape'][-1] != out_shape:
+        raise ValueError(
+            f"Network output shape {kwargs['shape'][-1]} != data output shape {out_shape}"
         )
-    elif kwargs['dims'][-1] != out_size:
-        log.error(f"Network output filters (num={kwargs['dims'][-1]}) has not been reduced, "
-                  'reshape with output = [-1] may be missing')
 
     return file['layers'], module_list
 