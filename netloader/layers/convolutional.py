Added layer reference for Linear and Reshape for output estimation. Added Pytorch to function to BaseNetwork. Changed ConvTranspose default padding to 0. Fixed runaway KL loss by Sample overwriting KL loss.

netloader/layers/convolutional.py
@@ -1,7 +1,7 @@
 """
 Convolutional network layers
 """
-from typing import Any
+from typing import Any, Type
 
 import torch
 import numpy as np
@@ -69,9 +69,9 @@ class Conv(BaseLayer):
         self._dropout: float = dropout
         padding_: int | str | list[int] = padding
         shape: list[int] = shapes[-1].copy()
-        conv: nn.Module
-        dropout_: nn.Module
-        batch_norm_: nn.Module
+        conv: Type[nn.Module]
+        dropout_: Type[nn.Module]
+        batch_norm_: Type[nn.Module]
 
         # Check for errors and calculate same padding
         if isinstance(padding, str):
@@ -252,7 +252,7 @@ class ConvTranspose(BaseLayer):
             stride: int | list[int] = 1,
             out_padding: int | list[int] = 0,
             dilation: int | list[int] = 1,
-            padding: int | str | list[int] = 'same',
+            padding: int | str | list[int] = 0,
             dropout: float = 0.1,
             **kwargs: Any):
         """
@@ -279,7 +279,7 @@ class ConvTranspose(BaseLayer):
             Padding applied to the output
         dilation : int | list[int], default = 1
             Spacing between kernel points
-        padding : int | str | list[int], default = 'same'
+        padding : int | str | list[int], default = 0
             Inverse of convolutional padding which removes rows from each dimension in the output
         dropout : float, default =  0.1
             Probability of dropout
@@ -294,9 +294,9 @@ class ConvTranspose(BaseLayer):
         self._slice: np.ndarray = np.array([slice(None)] * len(shapes[-1][1:]))
         padding_: int | str | list[int] = padding
         shape: list[int] = shapes[-1].copy()
-        transpose: nn.Module
-        dropout_: nn.Module
-        batch_norm_: nn.Module
+        transpose: Type[nn.Module]
+        dropout_: Type[nn.Module]
+        batch_norm_: Type[nn.Module]
 
         # Check for errors and calculate same padding
         if isinstance(padding, str):

netloader/layers/linear.py
@@ -36,6 +36,7 @@ class Linear(BaseLayer):
             shapes: list[list[int]],
             features: int | None = None,
             factor: int | None = None,
+            layer: int | None = None,
             batch_norm: bool = False,
             activation: bool = True,
             dropout: float = 0.01,
@@ -51,8 +52,11 @@ class Linear(BaseLayer):
             Number of output features for the layer,
             if out_shape from kwargs and factor is provided, features will not be used
         factor : float, optional
-            Output features is equal to the factor of the network's output,
-            will be used if provided, else features will be used
+            Output features is equal to the factor of the network's output, or if layer is provided,
+            which layer to be relative to, will be used if provided, else features will be used
+        layer : int, optional
+            If factor is True, which layer for factor to be relative to, if None, network output
+            will be used
         batch_norm : bool, default = False
             If batch normalisation should be used
         activation : bool, default = True
@@ -66,8 +70,9 @@ class Linear(BaseLayer):
         self._activation: bool = activation
         self._batch_norm: bool = batch_norm
         self._dropout: float = dropout
-        self._in_shape: list[int] = shapes[-1].copy()
         self._out_shape: list[int]
+        self._in_shape: list[int] = shapes[-1].copy()
+        target: list[int] = shapes[layer] if layer is not None else net_out
 
         # Remove channels dimension
         if len(self._in_shape) > 1:
@@ -75,7 +80,7 @@ class Linear(BaseLayer):
 
         # Number of features can be defined by either a factor of the output size or explicitly
         if factor:
-            features = max(1, int(np.prod(net_out) * factor))
+            features = max(1, int(np.prod(target) * factor))
 
         assert isinstance(features, int)
         self.layers.append(nn.Linear(
@@ -207,7 +212,7 @@ class Sample(BaseLayer):
             Leftover parameters to pass to base layer for checking
         """
         super().__init__(idx=idx, **kwargs)
-        self.sample_layer: torch.distributions.Distribution = torch.distributions.Normal(
+        self.sample_layer: torch.distributions.Normal = torch.distributions.Normal(
             torch.tensor(0.).to(self._device),
             torch.tensor(1.).to(self._device),
         )
@@ -241,7 +246,7 @@ class Sample(BaseLayer):
         std: Tensor = torch.exp(x[:, split:2 * split])
         x = mean + std * self.sample_layer.sample(mean.shape)
 
-        net.kl_loss += net.kl_loss_weight * 0.5 * torch.mean(
+        net.kl_loss = net.kl_loss_weight * 0.5 * torch.mean(
             mean ** 2 + std ** 2 - 2 * torch.log(std) - 1
         )
         return x
@@ -293,7 +298,7 @@ class Upsample(BaseLayer):
             Leftover parameters to pass to base layer for checking
         """
         super().__init__(idx=idx, **kwargs)
-        scale_arg: dict[str, list[int] | tuple[float, ...]]
+        scale_arg: dict[str, tuple[int, ...] | tuple[float, ...]]
         modes: dict[str, list[int]] = {
             'nearest': [2, 3, 4],
             'linear': [2],
@@ -314,7 +319,7 @@ class Upsample(BaseLayer):
             scale = (scale,) * len(shapes[-1][1:])
 
         if shape:
-            scale_arg = {'size': shape}
+            scale_arg = {'size': tuple(shape)}
             shapes.append(shape)
         else:
             scale_arg = {'scale_factor': scale}

netloader/layers/pooling.py
@@ -1,7 +1,7 @@
 """
 Pooling network layers
 """
-from typing import Any
+from typing import Any, Type
 
 import numpy as np
 from torch import nn, Tensor
@@ -50,7 +50,7 @@ class AdaptivePool(BaseLayer):
         super().__init__(**kwargs)
         self._channels: bool = channels
         self._mode: str = mode
-        adapt_pool: nn.Module
+        adapt_pool: Type[nn.Module]
 
         self._check_shape(shapes[-1])
         self._check_adapt_pool(shapes[-1], shape)
@@ -162,7 +162,7 @@ class Pool(BaseLayer):
         shape: list[int]
         modes: tuple[str, str] = ('max', 'average')
         avg_kwargs: dict[str, bool] = {}
-        pool: nn.Module
+        pool: Type[nn.Module]
 
         # Check for errors and calculate same padding
         if isinstance(padding, str):

netloader/layers/recurrent.py
@@ -1,7 +1,7 @@
 """
 Recurrent network layers
 """
-from typing import Any
+from typing import Any, Type
 
 import torch
 import numpy as np
@@ -72,7 +72,7 @@ class Recurrent(BaseLayer):
         self._bidirectional: str | None = bidirectional
         self._options: list[str | None] = [None, 'sum', 'mean', 'concatenate']
         shape: list[int] = [filters, int(np.prod(shapes[-1][1:]))]
-        modes: dict[str, nn.RNNBase] = {'rnn': nn.RNN, 'lstm': nn.LSTM, 'gru': nn.GRU}
+        modes: dict[str, Type[nn.RNNBase]] = {'rnn': nn.RNN, 'lstm': nn.LSTM, 'gru': nn.GRU}
         recurrent: nn.Module
 
         self._check_options('bidirectional', self._bidirectional, set(self._options))

netloader/network.py
@@ -90,7 +90,7 @@ class Network(nn.Module):
         self.kl_loss: Tensor = torch.tensor(0.)
         self.net: nn.ModuleList
         self.optimiser: optim.Optimizer
-        self.scheduler: optim.lr_scheduler.LRScheduler
+        self.scheduler: optim.lr_scheduler.ReduceLROnPlateau
 
         if '.json' not in self.name:
             self.name += '.json'

netloader/networks/base.py
@@ -5,7 +5,7 @@ import os
 import pickle
 import logging as log
 from time import time
-from typing import Any
+from typing import Any, Self
 
 import torch
 import numpy as np
@@ -179,10 +179,7 @@ class BaseNetwork:
             loss.backward()
             self.net.optimiser.step()
 
-            # Prevents memory leaks
-            self.net.kl_loss = self.net.kl_loss.detach()
-
-    def _update_scheduler(self, scheduler: torch.optim.lr_scheduler.LRScheduler) -> None:
+    def _update_scheduler(self, scheduler: torch.optim.lr_scheduler.ReduceLROnPlateau) -> None:
         """
         Updates the scheduler for the network
 
@@ -384,8 +381,8 @@ class BaseNetwork:
         if self.transform is not None:
             data.iloc[:, 1:3] = data.iloc[:, 1:3] * self.transform[1] + self.transform[0]
 
-        data.columns = header[:len(data.columns)]
-        data = {key: np.stack(array).squeeze() for key, array in data.items()}
+        data.columns = pd.Index(header[:len(data.columns)])
+        data = {str(key): np.stack(array).squeeze() for key, array in data.items()}
         self._save_predictions(path, data)
         return data
 
@@ -404,6 +401,55 @@ class BaseNetwork:
             N predictions for the given data
         """
         return (self.net(data).detach().cpu().numpy(),)
+
+    def to(self, *args: Any, **kwargs: Any) -> Self:
+        r"""Move and/or cast the parameters and buffers.
+
+        This can be called as
+
+        .. function:: to(device=None, dtype=None, non_blocking=False)
+           :noindex:
+
+        .. function:: to(dtype, non_blocking=False)
+           :noindex:
+
+        .. function:: to(tensor, non_blocking=False)
+           :noindex:
+
+        .. function:: to(memory_format=torch.channels_last)
+           :noindex:
+
+        Its signature is similar to :meth:`torch.Tensor.to`, but only accepts
+        floating point or complex :attr:`dtype`\ s. In addition, this method will
+        only cast the floating point or complex parameters and buffers to :attr:`dtype`
+        (if given). The integral parameters and buffers will be moved
+        :attr:`device`, if that is given, but with dtypes unchanged. When
+        :attr:`non_blocking` is set, it tries to convert/move asynchronously
+        with respect to the host if possible, e.g., moving CPU Tensors with
+        pinned memory to CUDA devices.
+
+        See below for examples.
+
+        .. note::
+            This method modifies the module in-place.
+
+        Args:
+            device (:class:`torch.device`): the desired device of the parameters
+                and buffers in this module
+            dtype (:class:`torch.dtype`): the desired floating point or complex dtype of
+                the parameters and buffers in this module
+            tensor (torch.Tensor): Tensor whose dtype and device are the desired
+                dtype and device for all parameters and buffers in this module
+            memory_format (:class:`torch.memory_format`): the desired memory
+                format for 4D parameters and buffers in this module (keyword
+                only argument)
+
+        Returns:
+            Module: self
+        """
+        self.net = self.net.to(*args, **kwargs)
+        self._device, *_ = torch._C._nn._parse_to(*args, **kwargs)
+        return self
 
 
 def load_net(num: int, states_dir: str, net_name: str) -> BaseNetwork: